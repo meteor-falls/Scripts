5ed2bb3bd53338d7239b9a391775c6f4969d5ee7
TheUnknownOne <TheUnknownOne25@gmail.com>
2013-08-22 03:08:47 +0200

    Update commands.js

--- plugins/commands.js
+++ plugins/commands.js
@@ -602,7 +602,7 @@ function handleCommand(src, message, command, commandData, tar, chan) {
         var possibilities = [
             "<font color=navy><timestamp/><b>±RouletteBot: </b></font><b><font color=" + namecolor(src) + ">" + html_escape(sys.name(src)) + "</b></font> has spun a <font color=gray><b>" + sys.rand(1, 9002) + "</b></font> and won a <b><font color=red>" + sys.nature(sys.rand(1, 25)) + "</b></font> <b><font color=blue>" + sys.pokemon(numb) + "!<img src='pokemon:" + numb + "&gen=5' width='50'></b></font>",
             "<font color=navy><timestamp/><b>±RouletteBot: </b></font><b><font color=" + namecolor(src) + ">" + sys.name(src) + "</b></font> has spun a <font color=gray><b>" + sys.rand(1, 9002) + "</b></font> and won <b><font color=red>" + sys.item(num) + "! <img src='item:" + num + "'></b></font>",
-            "<font color=navy><timestamp/><b>±RouletteBot: </b></font><b><font color=" + namecolor(src) + ">" + sys.name(src) + "</b></font> has spun a <font color=gray><b>" + sys.rand(1, 9002) + "</b></font> and won <b><font color=red>" + randomEmote + "! <img src='item:" + EmoteList[randomEmote] + "'></b></font>"
+            "<font color=navy><timestamp/><b>±RouletteBot: </b></font><b><font color=" + namecolor(src) + ">" + sys.name(src) + "</b></font> has spun a <font color=gray><b>" + sys.rand(1, 9002) + "</b></font> and won <b><font color=red>" + randomEmote + "! " + EmoteList[randomEmote] + "</b></font>"
         ];
         sys.sendHtmlAll(possibilities[sys.rand(0, possibilities.length)], chan);
         return;
