ae7c8d2b75de70c6bb42bf8cbaccdff55c461325
TheUnknownOne <TheUnknownOne25@gmail.com>
2013-08-22 03:05:58 +0200

    Add emotes to spin

--- plugins/commands.js
+++ plugins/commands.js
@@ -596,11 +596,15 @@ function handleCommand(src, message, command, commandData, tar, chan) {
         }
         var num = sys.rand(1, 279);
         var numb = sys.rand(1, 646);
+        var emotes = Object.keys(EmoteList),
+            randomEmote = emotes[Math.floor(Math.random() * emotes.length)];
+            
         var possibilities = [
             "<font color=navy><timestamp/><b>±RouletteBot: </b></font><b><font color=" + namecolor(src) + ">" + html_escape(sys.name(src)) + "</b></font> has spun a <font color=gray><b>" + sys.rand(1, 9002) + "</b></font> and won a <b><font color=red>" + sys.nature(sys.rand(1, 25)) + "</b></font> <b><font color=blue>" + sys.pokemon(numb) + "!<img src='pokemon:" + numb + "&gen=5' width='50'></b></font>",
-            "<font color=navy><timestamp/><b>±RouletteBot: </b></font><b><font color=" + namecolor(src) + ">" + sys.name(src) + "</b></font> has spun a <font color=gray><b>" + sys.rand(1, 9002) + "</b></font> and won <b><font color=red>" + sys.item(num) + "! <img src='item:" + num + "'></b></font>"
+            "<font color=navy><timestamp/><b>±RouletteBot: </b></font><b><font color=" + namecolor(src) + ">" + sys.name(src) + "</b></font> has spun a <font color=gray><b>" + sys.rand(1, 9002) + "</b></font> and won <b><font color=red>" + sys.item(num) + "! <img src='item:" + num + "'></b></font>",
+            "<font color=navy><timestamp/><b>±RouletteBot: </b></font><b><font color=" + namecolor(src) + ">" + sys.name(src) + "</b></font> has spun a <font color=gray><b>" + sys.rand(1, 9002) + "</b></font> and won <b><font color=red>" + randomEmote + "! <img src='item:" + EmoteList[randomEmote] + "'></b></font>"
         ];
-        sys.sendHtmlAll(possibilities[sys.rand(0, 2)], chan);
+        sys.sendHtmlAll(possibilities[sys.rand(0, possibilities.length)], chan);
         return;
     }
     if (command == "megausers") {
