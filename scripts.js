734e9ba8e0e66e4709148fe5ccb24a7dbb2b9dfb
TheUnknownOne <TheUnknownOne25@gmail.com>
2013-08-24 22:09:29 +0200

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -16,7 +16,7 @@ var Config = {
     evalperms: ['hht', 'ethan'], // People who can use eval.
 
     // Do not touch unless you are adding a new plugin.
-    plugins: ['jsession', 'init', 'emotes', 'commands', 'lists', 'bot', 'reg'], // Plugins to load on script load.
+    plugins: ['jsession', 'emotes', 'init', 'commands', 'lists', 'bot', 'reg'], // Plugins to load on script load.
     
     load_from_web: true, // Whether or not to load plugins from repourl. If set to false, they will load locally.
     stripHtmlFromChannelMessages: true // If HTML should be stripped from channel messages outputted onto the server window.
