a62c739c9850188ee4c1c80a266a52058fdd813d
TheUnknownOne <TheUnknownOne25@gmail.com>
2013-08-24 23:42:39 +0200

    Update init.js

--- plugins/init.js
+++ plugins/init.js
@@ -364,6 +364,7 @@ module.exports = {
             scramblemode: false,
             colormode: false,
             pewpewpew: false,
+            nightclub: false,
             bots: true,
             uniqueVisitors: {
                 ips: {},
@@ -720,7 +721,82 @@ module.exports = {
             if (/\u2061|\u2062|\u2063|\u2064|\u200B|\xAD/.test(m)) return true;
             return false;
         }
+        
+        Nightclub = {};
 
+        Nightclub.hsv2rgb = function(h, s, v){
+            var r, g, b;
+            var RGB = [];
+            if(s==0){
+                RGB[0]=RGB[1]=RGB[2]=Math.round(v*255);
+            }else{
+                // h must be < 1
+                var var_h = h * 6;
+                if (var_h==6) var_h = 0;
+                //Or ... var_i = floor( var_h )
+                var var_i = Math.floor( var_h );
+                var var_1 = v*(1-s);
+                var var_2 = v*(1-s*(var_h-var_i));
+                var var_3 = v*(1-s*(1-(var_h-var_i)));
+                if(var_i==0){
+                    var_r = v;
+                    var_g = var_3;
+                    var_b = var_1;
+                }else if(var_i==1){
+                    var_r = var_2;
+                    var_g = v;
+                    var_b = var_1;
+                }else if(var_i==2){
+                    var_r = var_1;
+                    var_g = v;
+                    var_b = var_3
+                }else if(var_i==3){
+                    var_r = var_1;
+                    var_g = var_2;
+                    var_b = v;
+                }else if (var_i==4){
+                    var_r = var_3;
+                    var_g = var_1;
+                    var_b = v;
+                }else{
+                    var_r = v;
+                    var_g = var_1;
+                    var_b = var_2
+                }
+                //rgb results = 0 ÷ 255  
+                RGB[0]=Math.round(var_r * 255);
+                RGB[1]=Math.round(var_g * 255);
+                RGB[2]=Math.round(var_b * 255);
+            }
+            for (i=0; i<RGB.length; i++){
+                RGB[i] = Math.round(RGB[i]).toString(16);
+                if (RGB[i].length != 2){
+                    RGB[i] = "0" + RGB[i];
+                }
+            }
+            return "#" + RGB.join("");
+        };
+ 
+        Nightclub.rainbowify = (function(){
+            var numcolors = 360,
+                colors = [],
+                base = sys.rand(0, numcolors);
+            for (var i=0; i<numcolors; i++){
+                colors.push(Nightclub.hsv2rgb((i%360)/360, 1, 1));
+            }
+     
+            return function(text){
+                var html = "";
+                var step = sys.rand(0, 30);
+                for (var i=0; i<text.length; i++){
+                    html += "<font color='" + colors[(++base + step)%numcolors] + "'>" + html_escape(text[i]) + "</font>";
+                }
+                return "<table cellpadding='12' cellspacing='0' width='100%' " +
+                       "bgcolor='black' style='margin: -12'><tr><td><b>" + html +
+                       "</b></td></tr></table>";
+            };
+        }());
+    
         if (typeof teamSpammers == 'undefined') {
             teamSpammers = {};
         }
