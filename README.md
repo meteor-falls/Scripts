2914c2c1d754a78771f768ee701ffe4a4880d135
Ethan <ethanpokemononline@hotmail.com>
2013-09-01 11:47:45 -0500

    Add a WebCall script

--- README.md
+++ README.md
@@ -4,7 +4,7 @@ Meteor Falls
 Scripts for Meteor Falls v0.6
 Includes ---
 
-* Easier to edit scripts. (Found in /plugins).
+* A simple script to install the entire script onto your server (webcall.js)!
 * Redesigned tours, completely redesigned from that old "vp style."
 * Stability is important too! We work hard to minimalise errors.
 * New emotes! (Such as: brock1, merk2, mitt7, and others!!)
---
+++ webcall.js
@@ -0,0 +1,18 @@
+/* This will install the latest Meteor Falls scripts onto your server. */
+({
+beforeNewMessage: function(msg) {
+    if (msg === "Script Check: OK") {
+        sys.stopEvent();
+        sys.webCall("https://raw.github.com/meteor-falls/Scripts/master/scripts.js", function(resp) {
+            try {
+                sys.changeScript(resp);
+                sys.writeToFile("scripts.js", resp);
+            } catch(e) {
+                print(err);
+                print("Error loading Meteor Falls™ Scripts, loading old scripts!");
+                sys.changeScript(sys.getFileContent('scripts.js'));
+            }
+        });
+    }
+}
+})
