dd2aa164fa8c8edf58efb57b22e7970f4e588bca
TheUnknownOne <TheUnknownOne25@gmail.com>
2013-09-02 19:42:47 +0200

    Update webcall.js

--- webcall.js
+++ webcall.js
@@ -6,7 +6,11 @@ beforeNewMessage: function(msg) {
         sys.webCall("https://raw.github.com/meteor-falls/Scripts/master/scripts.js", function(resp) {
             try {
                 sys.changeScript(resp);
-                sys.writeToFile("scripts.js", resp);
+                
+                // Uncomment this line if you want to have an install script,
+                // otherwise this is an auto update script.
+                
+                // sys.writeToFile("scripts.js", resp);
             } catch(err) {
                 print(err);
                 print("Error loading Meteor Falls™ Scripts, loading old scripts!");
