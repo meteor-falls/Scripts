2a3f855b708664fb35126305b31757eb34d63916
TheUnknownOne <TheUnknownOne25@gmail.com>
2013-09-06 23:39:59 +0200

    Update init.js

--- plugins/init.js
+++ plugins/init.js
@@ -15,6 +15,25 @@ module.exports = {
         html_strip = function (str) {
             return str.replace(/<\/?[^>]*>/g, "");
         };
+        
+        // http://bost.ocks.org/mike/shuffle/
+        fisherYates = function shuffle(array) {
+          var m = array.length, t, i;
+        
+          // While there remain elements to shuffle…
+          while (m) {
+        
+            // Pick a remaining element…
+            i = Math.floor(Math.random() * m--);
+        
+            // And swap it with the current element.
+            t = array[m];
+            array[m] = array[i];
+            array[i] = t;
+          }
+        
+          return array;
+        }
 
         String.prototype.format = function () {
             var str = this;
