f32b18154ba7b5739627978257c3df4327b46831
TheUnknownOne <TheUnknownOne25@gmail.com>
2013-09-06 23:46:05 +0200

    Update commands.js

--- plugins/commands.js
+++ plugins/commands.js
@@ -1876,7 +1876,7 @@ addCommand(3, ["scramblemotes"], function (src, command, commandData, tar, chan)
         i;
     
     for (i in EmoteList) {
-        if (i === "__display__") {
+        if (i !== "__display__") {
             values.push(EmoteList[i]);
         }
     }
