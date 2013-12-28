167b6a973f65eaa98f710a9eb2b6d8c4082d31b1
Derek <viperspitblade@gmail.com>
2013-12-27 20:47:19 -0500

    Update commands.js

--- plugins/commands.js
+++ plugins/commands.js
@@ -2149,28 +2149,6 @@
     addMaintainerCommand("id", function (src, command, commandData, tar, chan) {
         bot.sendMessage(src, commandData + "'s id is <b>" + (sys.id(commandData) || "~Unknown~") + "</b>.", chan);
     });
-    
-    addMaintainerCommand("fsaym", function (src, command, commandData, tar, chan) {
-        var parts = commandData.split(':'),
-            target = parts[0],
-            msg = Utils.cut(parts, 1, ':').trim(),
-            intid;
-        
-        if (!isNaN((intid = parseInt(target, 10)))) {
-            tar = intid;
-        } else {
-            tar = sys.id(target);
-        }
-        
-        if (!tar) {
-            return bot.sendMessage(src, target + " doesn't appear to be anyone who's online right now.", chan);
-        }
-        if (!msg) {
-            return bot.sendMessage(src, "Hmm, no message?", chan);
-        }
-        
-        script.beforeChatMessage(tar, msg, chan);
-    });
 
     /* Exports & metadata */
     module.exports = {
