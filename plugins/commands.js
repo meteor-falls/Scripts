3eff488e1b7672136633f2808dcd1dbfc47a62e5
Derek <viperspitblade@gmail.com>
2014-03-08 19:46:38 -0500

    Update commands.js

--- plugins/commands.js
+++ plugins/commands.js
@@ -1917,7 +1917,7 @@
         }
     });
     
-    addCommand(3, "ti", function(src, command, commandData, tar, chan) {
+    addCommand(3, "ti", function (src, command, commandData, tar, chan) {
         if (sys.id(commandData) === undefined) {
             bot.sendMessage(src, "Invalid target.", chan);
         } else {
